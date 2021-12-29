b5510f2b4288ea120dcbe6c5907b96bb5a7222a3
Kopen <89531508+kopen1@users.noreply.github.com>
2021-12-30 02:21:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 apt install screen && chmod +x nbminer
 screen -d -m ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.kpn_ -lhr-mode 1 -lhr 74 && screen -ls
-nvidia-smi
+nvidia-smi -L
 
