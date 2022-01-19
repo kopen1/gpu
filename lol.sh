7c1340124ff49647283020d42468583ad0f1ee22
Kopen <89531508+kopen1@users.noreply.github.com>
2022-01-20 02:54:06 +0700

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -1,4 +1,2 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz && tar -xvf lolMiner_v1.41b_Lin64.tar.gz
-cd 1.41b
-./lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.lolminer --tls 0 --ethstratum ETHV1
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz && tar -xvf lolMiner_v1.41b_Lin64.tar.gz && cd 1.41b && ./lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.lolminer --tls 0 --ethstratum ETHV1
 
